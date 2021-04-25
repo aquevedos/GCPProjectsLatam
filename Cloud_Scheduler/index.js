a5d4dde235a2767c902772e64849176c7bb05b87
aquevedos91 <aquevedos@alicorp.com.pe>
2021-04-25 18:23:09 -0500

    6to Cambio

--- Cloud_Scheduler/index.js
+++ Cloud_Scheduler/index.js
@@ -4,43 +4,38 @@ const compute = new Compute();
 const default_zone = 'us-west1-b'
 
 exports.startVM = function startVM(req, res) {
+    
     try
     {
-        var zone = compute.zone(default_zone);
-        var vms_start = zone.vm('dev-instance');
-        vms_start.start(function(err, operation, apiResponse) 
+    var zone = compute.zone(default_zone);
+    var vm_start = zone.vm('dev-instance');
+    vm_start.start(function(err, operation, apiResponse) 
             {
                 console.log('Successfully started');
             });
-        res.status(200).send('Success!!');
     }
     catch (err) {
     console.log(err);
     callback(err);
     }
-
-    
 
 };
 
 
 
 exports.stopVM = function stopVM(req, res) {
-     try
-    {
-         var zone = compute.zone(default_zone);
-         var vms_stop = zone.vm('dev-instance');
-         vms_stop.stop(function(err, operation, apiResponse) {
+    try{
+
+    var zone = compute.zone(default_zone);
+    var vm_stop = zone.vm('dev-instance');
+    vm_stop.stop(function(err, operation, apiResponse) {
                 console.log('Successfully stopped');
             });
-         res.status(200).send('Server usually takes around 10 minutes to full stop');
+
     }
     catch (err) {
     console.log(err);
     callback(err);
     }
-
-   
-
 
 };
